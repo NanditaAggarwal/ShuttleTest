40b1fb54fb386f73bcb569dfbef9726b59299772
NanditaAggarwal <naggarwal@bbns.org>
2023-05-11 14:56:37 -0400

    Initial Commit

---
+++ ShuttleTest/ShuttleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuttleTestApp.swift
+//  ShuttleTest
+//
+//  Created by Nandita Aggarwal on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ShuttleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
